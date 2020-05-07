7baf9e9f5fd9d106825c987ffb70bab65841feac
swarzzy <hbr.tzuf@yandex.ru>
2020-05-07 23:02:15 +0700

    Added screenshot

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 
 use std::fs::File;
 use std::io::Write;
+use std::io;
+
+use std::mem::size_of;
 
 mod math;
 mod shapes;
@@ -12,6 +15,85 @@ use math::*;
 use shapes::*;
 use materials::*;
 use camera::*;
+
+#[derive(Copy, Clone, Default)]
+#[repr(C, packed)]
+struct BitmapHeader {
+    file_type: u16,
+    file_size: u32,
+    reserved1: u16,
+    reserved2: u16,
+    bitmap_offset: u32,
+    size: u32,
+    width: i32,
+    height: i32,
+    planes: u16,
+    bits_per_pixel: u16,
+    compression: u32,
+    size_of_bitmap: u32,
+    h_resolution: i32,
+    v_resolution: i32,
+    colors_used: u32,
+    colors_important: u32
+}
+
+impl  BitmapHeader {
+    fn make() -> Self {
+        Self {
+            file_type: 0x4d42,
+            size: (size_of::<Self>() - 14) as u32,
+            bitmap_offset: size_of::<Self>() as u32,
+            planes: 1,
+            bits_per_pixel: 32,
+            compression: 0,
+            .. Default::default()
+        }
+    }
+}
+
+#[derive(Default)]
+struct Image {
+    width: u32,
+    height: u32,
+    pixels: Vec<u32>
+}
+
+impl Image {
+    fn make(width: u32, height: u32) -> Self {
+        Self {
+            width,
+            height,
+            pixels: vec![0; (width * height) as usize]
+        }
+    }
+
+    fn pixel(&mut self, x: u32, y: u32) -> Option<&mut u32> {
+        self.pixels.get_mut((y * self.width + x) as usize)
+    }
+
+    fn write_as_bmp(&self, filename: &str) -> io::Result<()> {
+        let pixels_size = self.width * self.height * size_of::<u32>() as u32;
+        assert!(pixels_size == (self.pixels.len() * size_of::<u32>()) as u32);
+
+        let mut header = BitmapHeader::make();
+        header.file_size = size_of::<BitmapHeader>() as u32 + pixels_size;
+        header.width = self.width as i32;
+        header.height = self.height as i32;
+        header.size_of_bitmap = pixels_size;
+
+        let mut file = File::create(filename)?;
+
+        let header_ptr: *const u8 = &header as *const _ as *const u8;
+        let header_size = size_of::<BitmapHeader>();
+        let header_slice = unsafe { std::slice::from_raw_parts(header_ptr, header_size) };
+        file.write_all(header_slice)?;
+
+        let bitmap_ptr = self.pixels.as_ptr() as *const u8;
+        let bitmap_slice = unsafe { std::slice::from_raw_parts(bitmap_ptr, pixels_size as usize) };
+        file.write_all(bitmap_slice)?;
+        Ok(())
+    }
+}
 
 fn color<T>(geometry: &Vec<T>, r : Ray, depth: i32) -> v3 where T: Hit {
 
@@ -76,12 +158,14 @@ fn make_scene<'a>() -> Vec<Sphere> {
 }
 
 fn main() {
-    let width = 1920;
-    let height = 1080;
-    let spp = 30;
+    let width = 200;
+    let height = 100;
+    let spp = 15;
 
-    let mut file = File::create("out.ppm").unwrap();
-    write!(&mut file, "P3\n{} {}\n255\n", width, height).unwrap();
+    let mut image = Image::make(width, height);
+
+//    let mut file = File::create("out.ppm").unwrap();
+//    write!(&mut file, "P3\n{} {}\n255\n", width, height).unwrap();
 
     let spheres = make_scene();
     let camera = Camera::default();
@@ -101,11 +185,15 @@ fn main() {
             let pixel_color = pixel_color / spp as f32;
             let pixel_color = linear_to_srgb(pixel_color);
 
-            let r = (255.99f32 * pixel_color.x) as i32;
-            let g = (255.99f32 * pixel_color.y) as i32;
-            let b = (255.99f32 * pixel_color.z) as i32;
+            let r = (255.99f32 * pixel_color.x) as u32;
+            let g = (255.99f32 * pixel_color.y) as u32;
+            let b = (255.99f32 * pixel_color.z) as u32;
 
-            write!(&mut file, "{} {} {}\n", r, g, b).unwrap();
+            let pixel = image.pixel(x, y).unwrap();
+            *pixel = (r << 16) | (g << 8) | b;
+
+            image.write_as_bmp("out.bmp").unwrap();
+//            write!(&mut file, "{} {} {}\n", r, g, b).unwrap();
         }
     }
 }
